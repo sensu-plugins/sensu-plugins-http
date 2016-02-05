ce6ce87d9687d85c159f3c82cf53ed7e8e79861f
Leon Gibat <brendan.gibat@gmail.com>
2016-02-05 08:59:33 -0500

    fixing rescue

--- lib/sensu-plugins-http/common.rb
+++ lib/sensu-plugins-http/common.rb
@@ -22,7 +22,7 @@ module Common
               resp = s3.get_object(bucket:config[:s3_config_bucket], key:config[:s3_config_key])
               s3_config = JSON.parse(resp.body.read, symbolize_names: true)
               config.merge!(s3_config)
-          rescue => e
+          rescue
               critical 'Error getting config file from s3'
           end
       end
