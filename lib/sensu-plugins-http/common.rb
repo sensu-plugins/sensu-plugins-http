070b511d1b9416bb6c38c9fcc812bcf4a2088402
Leon Gibat <brendan.gibat@gmail.com>
2016-02-05 09:14:36 -0500

    adding in .editorcfg to standardize line spacing for rubocop warnings

--- lib/sensu-plugins-http/common.rb
+++ lib/sensu-plugins-http/common.rb
@@ -16,15 +16,15 @@ module Common
   end
 
   def merge_s3_config
-      unless config[:s3_config_bucket].nil? && !config[:s3_config_key].nil?
-          s3 = Aws::S3::Client.new
-          begin
-              resp = s3.get_object(bucket:config[:s3_config_bucket], key:config[:s3_config_key])
-              s3_config = JSON.parse(resp.body.read, symbolize_names: true)
-              config.merge!(s3_config)
-          rescue
-              critical 'Error getting config file from s3'
-          end
+    unless config[:s3_config_bucket].nil? && !config[:s3_config_key].nil?
+      s3 = Aws::S3::Client.new
+      begin
+        resp = s3.get_object(bucket:config[:s3_config_bucket], key:config[:s3_config_key])
+        s3_config = JSON.parse(resp.body.read, symbolize_names: true)
+        config.merge!(s3_config)
+      rescue
+        critical 'Error getting config file from s3'
       end
+    end
   end
 end
