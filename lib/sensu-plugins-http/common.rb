049e52b29fabf2a1d386fceabb6d36c9d3c047b0
Leon Gibat <brendan.gibat@gmail.com>
2016-02-05 09:15:23 -0500

    fixing spacing

--- lib/sensu-plugins-http/common.rb
+++ lib/sensu-plugins-http/common.rb
@@ -19,7 +19,7 @@ module Common
     unless config[:s3_config_bucket].nil? && !config[:s3_config_key].nil?
       s3 = Aws::S3::Client.new
       begin
-        resp = s3.get_object(bucket:config[:s3_config_bucket], key:config[:s3_config_key])
+        resp = s3.get_object(bucket: config[:s3_config_bucket], key: config[:s3_config_key])
         s3_config = JSON.parse(resp.body.read, symbolize_names: true)
         config.merge!(s3_config)
       rescue
