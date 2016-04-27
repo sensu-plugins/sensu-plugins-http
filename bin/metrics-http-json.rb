30bf97dd78e4761f993b7eeb597d9c04aca901d4
Matt Jones <mattjones@yieldbot.com>
2016-04-26 23:11:32 -0400

    fix rubocop issues

--- bin/metrics-http-json.rb
+++ bin/metrics-http-json.rb
@@ -52,7 +52,7 @@ class HttpJsonGraphite < Sensu::Plugin::Metric::CLI::Graphite
          description: 'Metric naming scheme',
          short: '-s SCHEME',
          long: '--scheme SCHEME',
-         default: "#{Socket.gethostname}"
+         default: Socket.gethostname.to_s
 
   option :metric,
          description: 'Metric/JSON key pair ex:Connections::numConnections',
@@ -65,12 +65,12 @@ class HttpJsonGraphite < Sensu::Plugin::Metric::CLI::Graphite
          long: '--object OBJECT'
 
   def run
-    scheme = "#{config[:scheme]}"
-    metric_pair_input = "#{config[:metric]}"
+    scheme = config[:scheme].to_s
+    metric_pair_input = config[:metric].to_s
     if config[:object]
-      object = "#{config[:object]}"
+      object = config[:object].to_s
     end
-    url = URI.encode("#{config[:url]}")
+    url = URI.encode(config[:url].to_s)
     begin
       r = RestClient.get url
       metric_pair_array = metric_pair_input.split(/,/)
@@ -89,10 +89,10 @@ class HttpJsonGraphite < Sensu::Plugin::Metric::CLI::Graphite
           end
         end
       end
-      rescue Errno::ECONNREFUSED
-        critical "#{config[:url]} is not responding"
-      rescue RestClient::RequestTimeout
-        critical "#{config[:url]} Connection timed out"
+    rescue Errno::ECONNREFUSED
+      critical "#{config[:url]} is not responding"
+    rescue RestClient::RequestTimeout
+      critical "#{config[:url]} Connection timed out"
     end
     ok
   end
